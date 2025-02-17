3849d23cf6e534b420e3f23e06f6cc5e88c9c801
hignandlight <junhyunghl126@gmail.com>
2025-02-17 20:03:50 +0900

    Initialize the spring mvc project

---
+++ src/main/kotlin/com/parkjunhyung/IryeokFitAi/IryeokFitAiApplication.kt
@@ -0,0 +1,11 @@
+package com.parkjunhyung.IryeokFitAi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IryeokFitAiApplication
+
+fun main(args: Array<String>) {
+	runApplication<IryeokFitAiApplication>(*args)
+}
---
+++ src/test/kotlin/com/parkjunhyung/IryeokFitAi/IryeokFitAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.parkjunhyung.IryeokFitAi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IryeokFitAiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

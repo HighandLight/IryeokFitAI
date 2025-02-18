9e6ce5847bb51c12c6c5dac2bf13fa6fcdf75e6f
hignandlight <junhyunghl126@gmail.com>
2025-02-18 17:24:42 +0900

    Refactor ReportServiceTest to mock S3 integration

--- src/test/kotlin/com/parkjunhyung/IryeokFitAi/service/ReportServiceTest.kt
+++ src/test/kotlin/com/parkjunhyung/IryeokFitAi/service/ReportServiceTest.kt
@@ -2,26 +2,26 @@ package com.parkjunhyung.IryeokFitAi.service
 
 import com.parkjunhyung.IryeokFitAi.repository.ReportRepository
 import com.parkjunhyung.IryeokFitAi.repository.entity.Report
+import io.awspring.cloud.s3.S3Template
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class ReportServiceTest {
-
     private val reportRepository: ReportRepository = mockk()
+    private val s3Template: S3Template = mockk()  // Mock S3Template
+    private val bucketName = "iryeokfit"
 
-    private val reportService = ReportService(reportRepository)
+    private val reportService = ReportService(reportRepository, s3Template, bucketName)
 
     @Test
     fun `createReport() should create a new report and return it`() {
-        val report = Report(name = "Kyle")
+        val report = Report(name = "Luke")
         every { reportRepository.save(any()) } answers { firstArg() }
 
         val newReport = reportService.createReport(report)
 
-        val expected = report.name
-
-        assertEquals(expected, newReport.name)
+        assertEquals(report.name, newReport.name)
     }
 }

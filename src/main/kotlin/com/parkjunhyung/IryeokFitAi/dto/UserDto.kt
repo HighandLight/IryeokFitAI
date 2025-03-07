ec95b20e559c2cb9d36ea75754a57fe15ed88291
hignandlight <junhyunghl126@gmail.com>
2025-03-07 15:47:17 +0900

    Refactor User entity and service logic

--- src/main/kotlin/com/parkjunhyung/IryeokFitAi/dto/UserDto.kt
+++ src/main/kotlin/com/parkjunhyung/IryeokFitAi/dto/UserDto.kt
@@ -1,8 +1,18 @@
 package com.parkjunhyung.IryeokFitAi.dto
 
 import com.parkjunhyung.IryeokFitAi.repository.entity.User
+import com.parkjunhyung.IryeokFitAi.repository.entity.ENUM.UserStatus
 
-data class UserDto(val name: String, val email: String, val password: String)
+data class UserDto(
+    val name: String,
+    val email: String,
+    val phoneNumber: String,
+    val status: UserStatus
+)
 
-fun User.toUserDto() = UserDto(name = name, email = email, password = password )
-
+fun User.toUserDto() = UserDto(
+    name = name,
+    email = email,
+    phoneNumber = phoneNumber,
+    status = status,
+)

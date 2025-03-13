d578dfdf05d817c156e36c08338f782d9dd72d71
hignandlight <junhyunghl126@gmail.com>
2025-03-14 00:53:30 +0900

    Implement JWT authentication(token generation, validation, and user authentication)

--- src/main/kotlin/com/parkjunhyung/IryeokFitAi/request/CreateUserRequest.kt
+++ src/main/kotlin/com/parkjunhyung/IryeokFitAi/request/CreateUserRequest.kt
@@ -1,5 +1,6 @@
 package com.parkjunhyung.IryeokFitAi.request
 
+import com.parkjunhyung.IryeokFitAi.repository.entity.ENUM.UserRole
 import com.parkjunhyung.IryeokFitAi.repository.entity.User
 import com.parkjunhyung.IryeokFitAi.repository.entity.ENUM.UserStatus
 
@@ -7,7 +8,8 @@ data class CreateUserRequest (
     val name: String,
     val email: String,
     val password: String,
-    val phoneNumber: String
+    val phoneNumber: String,
+    val role: UserRole = UserRole.USER
 )
 
 fun CreateUserRequest.toUser() = User(
@@ -16,5 +18,6 @@ fun CreateUserRequest.toUser() = User(
     password = password,
     phoneNumber = phoneNumber,
     status = UserStatus.ACTIVATE, // TODO : 인증 구현 후 UNVERIFIED가 기본 default
+    role = role
 
 )
--- src/main/kotlin/com/parkjunhyung/IryeokFitAi/util/JwtUtil.kt
+++ src/main/kotlin/com/parkjunhyung/IryeokFitAi/util/JwtUtil.kt
@@ -2,25 +2,33 @@ package com.parkjunhyung.IryeokFitAi.util
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class JwtUtil {
-    private val secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+    @Value("\${spring.jwt.secret}")
+    private lateinit var secret: String
+    private val EXPIRATION_TIME: Long = 1000 * 60 * 60  // 우선 1시간으로
 
-    fun generateToken(email: String): String {
+
+    fun generateToken(email: String, role: String): String {
+        val key = Keys.hmacShaKeyFor(secret.toByteArray())
+
         return Jwts.builder()
             .setSubject(email)
+            .claim("role", role)
             .setIssuedAt(Date())
-            .setExpiration(Date(System.currentTimeMillis() + 1000 * 60 * 60))  // 우선 1시간으로
-            .signWith(secretKey)
+            .setExpiration(Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .signWith(key, SignatureAlgorithm.HS256)
             .compact()
     }
 
     fun validateToken(token: String): Boolean {
         return try {
-            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token)
+            val key = Keys.hmacShaKeyFor(secret.toByteArray())
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
             true
         } catch (e: Exception) {
             false
@@ -28,7 +36,14 @@ class JwtUtil {
     }
 
     fun extractEmail(token: String): String {
-        return Jwts.parserBuilder().setSigningKey(secretKey).build()
+        val key = Keys.hmacShaKeyFor(secret.toByteArray())
+        return Jwts.parserBuilder().setSigningKey(key).build()
             .parseClaimsJws(token).body.subject
+    }
+
+    fun extractRole(token: String): String {
+        val key = Keys.hmacShaKeyFor(secret.toByteArray())
+        return Jwts.parserBuilder().setSigningKey(key).build()
+            .parseClaimsJws(token).body["role"] as String
     }
 }

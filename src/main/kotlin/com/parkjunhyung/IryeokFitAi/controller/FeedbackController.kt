8c73576b190f8a3435edbe1a4a09d85c20a41312
hignandlight <junhyunghl126@gmail.com>
2025-07-01 20:14:18 +0900

    Update report status to COMPLETED after feedback generation

--- src/main/kotlin/com/parkjunhyung/IryeokFitAi/controller/FeedbackController.kt
+++ src/main/kotlin/com/parkjunhyung/IryeokFitAi/controller/FeedbackController.kt
@@ -3,15 +3,19 @@ package com.parkjunhyung.IryeokFitAi.controller
 import com.parkjunhyung.IryeokFitAi.dto.FeedbackDto
 import com.parkjunhyung.IryeokFitAi.dto.toFeedbackDto
 import com.parkjunhyung.IryeokFitAi.repository.entity.ENUM.FeedbackStatus
+import com.parkjunhyung.IryeokFitAi.repository.entity.ENUM.ReportStatus
 import com.parkjunhyung.IryeokFitAi.repository.entity.Feedback
 import com.parkjunhyung.IryeokFitAi.service.FeedbackService
+import com.parkjunhyung.IryeokFitAi.service.ReportService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/feedbacks")
 class FeedbackController (
-    private val feedbackService: FeedbackService
+    private val feedbackService: FeedbackService,
+    private val reportService: ReportService // IRYEOKFIT-020 관련, 리포트 Status 변경 위한 의존성
+
 ) {
     @GetMapping("/{reportId}")
     fun getFeedbacksByReportId(@PathVariable reportId: Long): ResponseEntity<List<FeedbackDto>> {
@@ -34,7 +38,11 @@ class FeedbackController (
     @PostMapping("/generate/{reportId}")
     fun generateFeedbackForReport(@PathVariable reportId: Long): ResponseEntity<List<FeedbackDto>> {
         val newFeedbacks = feedbackService.generateFeedback(reportId)
+
+        reportService.updateReportStatus(reportId, ReportStatus.COMPLETED) // ReportStatus 변경(IRYEOKFIT-020 이슈)
+
         val dtoList = newFeedbacks.map { it.toFeedbackDto() }
         return ResponseEntity.ok(dtoList)
     }
+
 }
